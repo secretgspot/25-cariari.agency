0a764676c099cbb39ea0ccd6ec37fc965c8bae7a
Secret G Spot <secregspot@gmail.com>
2025-07-06 20:55:19 -0600

    Nav refactored

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -5,23 +5,11 @@
 	/** @type {{ is_logged_in: boolean; fixed?: boolean; basic?: boolean; url: any; }} */
 	let { fixed = false, basic = false, url, ...rest } = $props();
 	// console.log('Nav props', { page, fixed, basic, url, ...rest });
-
-	let open = $state(false);
 </script>
 
 <!-- {JSON.stringify(is_logged_in, null, 2)} -->
 
-<nav class:fixed class:basic class:open {...rest}>
-	<div class="icon">
-		<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" viewBox="0 0 485 485">
-			<path
-				fill="currentColor"
-				d="M352.5 207.5a35 35 0 1 0 0 70 35 35 0 0 0 0-70zm-220 0a35 35 0 1 0 0 70 35 35 0 0 0 0-70zm110 0a35 35 0 1 0 0 70 35 35 0 0 0 0-70z" />
-			<path
-				fill="currentColor"
-				d="M414 71C368.2 25.2 307.3 0 242.5 0S116.8 25.2 71 71 0 177.7 0 242.5 25.2 368.2 71 414s106.7 71 171.5 71 125.7-25.2 171.5-71 71-106.7 71-171.5S459.8 116.8 414 71zM242.5 455C125.3 455 30 359.7 30 242.5S125.3 30 242.5 30 455 125.3 455 242.5 359.7 455 242.5 455z" />
-		</svg>
-	</div>
+<nav class:fixed class:basic {...rest}>
 	<div class="wrapper">
 		{#if page.data.is_admin}<span title="admin">🔥</span>{/if}
 		{#if page.url.pathname != '/'}
@@ -61,30 +49,17 @@
 
 <style>
 	nav {
-		display: grid;
-		justify-items: end;
-		justify-content: end;
 		position: absolute;
 		top: var(--padding-small);
 		right: var(--padding-small);
 		row-gap: var(--gap-extra-small);
 		user-select: none;
-		/* cursor: pointer; */
 		z-index: 3;
-	}
-	.icon {
-		width: 27px;
-		height: 27px;
-		border-radius: 50%;
-		outline: 6px solid hsl(var(--p) / 0.6);
-		background: hsl(var(--p) / 0.6);
-		backdrop-filter: blur(3px);
 	}
 	.wrapper {
-		display: none;
+		display: flex;
 		gap: var(--gap-extra-small);
 		list-style: none;
-		z-index: 3;
 		background: hsl(var(--p) / 0.9);
 		backdrop-filter: blur(3px);
 		padding: var(--padding-extra-small);
@@ -96,20 +71,5 @@
 	.basic {
 		position: static;
 		justify-self: end;
-	}
-	nav.open .icon + .wrapper {
-		display: flex;
-		flex-direction: column;
-		align-items: flex-end;
-		position: absolute;
-		top: 54px;
-	}
-	@media (min-width: 768px) {
-		.icon {
-			display: none;
-		}
-		.wrapper {
-			display: flex;
-		}
 	}
 </style>
